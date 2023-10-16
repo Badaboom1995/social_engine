6049e0e406063f51a2f9e5239728ce6e1ca130a8
Alexey <alexey@Alexeys-MacBook-Pro.local>
2023-10-17 00:51:33 +0800

    adq

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,8 +17,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
       <Script src='https://telegram.org/js/telegram-web-app.js' />
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
