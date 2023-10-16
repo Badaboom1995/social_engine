ceb4992964fb419c6a60ba5b1f4358de19fb938a
Alexey <alexey@Alexeys-MacBook-Pro.local>
2023-10-16 16:06:08 +0800

    asd

--- app/api/route.ts
+++ app/api/route.ts
@@ -7,11 +7,11 @@ const bot = new Telegraf(process.env.BOT_TOKEN || '')
 export async function POST(request: any) {
   // Do whatever you want
   try {
-    const res = JSON.stringify(request)
-    await bot.telegram.sendMessage('208165379', 'Hello World')
+    console.log(request)
+    await bot.telegram.sendMessage('208165379', request)
   } catch (error) {}
   // @ts-ignore
-  await bot.telegram.sendMessage('208165379', request)
-  console.log('got post request')
+  // await bot.telegram.sendMessage('208165379', request)
+  // console.log(request)
   return NextResponse.json({ request }, { status: 200 })
 }
