2bc55f04b7d3e429c34cb89714d05dcbf14c9e2a
Alexey <alexey@Alexeys-MacBook-Pro.local>
2023-10-16 15:05:43 +0800

    add telegraf

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,8 +1,12 @@
 import { NextResponse } from 'next/server'
+import { Telegraf } from 'telegraf'
+
+const bot = new Telegraf(process.env.BOT_TOKEN || '')
 
 // To handle a GET request to /api
 export async function GET(request: any) {
   // Do whatever you want
+  bot.telegram.sendMessage('208165379', 'Hello World')
   return NextResponse.json({ message: 'Root' }, { status: 200 })
 }
 
