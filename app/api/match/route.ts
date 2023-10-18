01dc11513efad84d097a505cab3c255d7f78d8bc
Badavoo <lordalurard@gmail.com>
2023-10-18 20:17:03 +0800

    asd

--- app/api/match/route.ts
+++ app/api/match/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import { runMatching } from '@/matching/matching'
 
-export async function GET() {
+export async function POST() {
   const res = await runMatching()
   return NextResponse.json(res, { status: 200 })
 }
