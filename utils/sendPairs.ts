791a4e3fc4e54bb354dd4b5eacf2f4781d649f4d
Badavoo <lordalurard@gmail.com>
2023-11-27 15:15:26 +0300

    fix

--- utils/sendPairs.ts
+++ utils/sendPairs.ts
@@ -12,59 +12,63 @@ function wait(ms: number) {
 export const sendPairs = async (pairs: BestPairType[]) => {
   await bot.telegram.sendMessage(badavooChatId, 'Start sending pairs')
   for (const pair of pairs) {
-    if (!pair.user.chat_id || !pair.partner.chat_id) {
+    try {
+      if (!pair.user.chat_id || !pair.partner.chat_id) {
+        await bot.telegram.sendMessage(
+          badavooChatId,
+          `Error: ${pair.user.telegram} ${pair.partner.telegram}`,
+        )
+        continue
+      }
+      await supabase.from('Pairs').insert([
+        {
+          user: pair.user.telegram,
+          partner: pair.partner.telegram,
+        },
+      ])
+      await bot.telegram.sendMessage(
+        pair.user.chat_id,
+        `Новая пара! @${pair.partner.telegram}`,
+        {
+          reply_markup: {
+            inline_keyboard: [
+              [
+                {
+                  text: 'Профиль',
+                  web_app: {
+                    url: `https://social-engine.vercel.app/profile?chatID=${pair.partner.chat_id}`,
+                  },
+                },
+              ],
+            ],
+          },
+        },
+      )
+      await wait(300)
+      await bot.telegram.sendMessage(
+        pair.partner.chat_id,
+        `Новая пара! @${pair.user.telegram}`,
+        {
+          reply_markup: {
+            inline_keyboard: [
+              [
+                {
+                  text: 'Профиль',
+                  web_app: {
+                    url: `https://social-engine.vercel.app/profile?chatID=${pair.user.chat_id}`,
+                  },
+                },
+              ],
+            ],
+          },
+        },
+      )
       await bot.telegram.sendMessage(
         badavooChatId,
-        `Error: ${pair.user.telegram} ${pair.partner.telegram}`,
+        `Sent: ${pair.user.telegram} ${pair.partner.telegram}`,
       )
-      continue
+    } catch (e) {
+      console.log('error', pair.user.telegram, pair.partner.telegram)
     }
-    await supabase.from('Pairs').insert([
-      {
-        user: pair.user.telegram,
-        partner: pair.partner.telegram,
-      },
-    ])
-    await bot.telegram.sendMessage(
-      pair.user.chat_id,
-      `Новая пара! @${pair.partner.telegram}`,
-      {
-        reply_markup: {
-          inline_keyboard: [
-            [
-              {
-                text: 'Профиль',
-                web_app: {
-                  url: `https://social-engine.vercel.app/profile?chatID=${pair.partner.chat_id}`,
-                },
-              },
-            ],
-          ],
-        },
-      },
-    )
-    await wait(300)
-    await bot.telegram.sendMessage(
-      pair.partner.chat_id,
-      `Новая пара! @${pair.user.telegram}`,
-      {
-        reply_markup: {
-          inline_keyboard: [
-            [
-              {
-                text: 'Профиль',
-                web_app: {
-                  url: `https://social-engine.vercel.app/profile?chatID=${pair.user.chat_id}`,
-                },
-              },
-            ],
-          ],
-        },
-      },
-    )
-    await bot.telegram.sendMessage(
-      badavooChatId,
-      `Sent: ${pair.user.telegram} ${pair.partner.telegram}`,
-    )
   }
 }
